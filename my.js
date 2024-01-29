796ab54d18fd08383cfab509221520f2b724f5b3
sdunlap1 <sdunlap1@mac.com>
2024-01-28 19:08:29 -0800

    Refactoring some functions

---
+++ my.js
@@ -0,0 +1,32 @@
+/* function double(arr) {
+  return arr.map(function(val) {
+    return val * 2;
+  });
+} */
+
+// ES2015 Arrow Functions Shorthand
+const double = (arr) => arr.map((val) => val * 2);
+
+/* function squareAndFindEvens(numbers){
+  var squares = numbers.map(function(num){
+    return num ** 2;
+  });
+  var evens = squares.filter(function(square){
+    return square % 2 === 0;
+  });
+  return evens;
+} */
+
+// Refactored to use all arrow functions
+// function squareAndFindEvens(numbers) {
+//   var squares = numbers.map((num) => {
+//     return num ** 2;
+//   });
+//   var evens = squares.filter((square) => {
+//     return square % 2 === 0;
+//   });
+//   return evens;
+// }
+
+const squareAndFindEvens = (numbers) =>
+  numbers.map((val) => val ** 2).filter((square) => square % 2 === 0);
